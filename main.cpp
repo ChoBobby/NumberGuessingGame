92c851a5f93371a270cf2bfe99fcabffae5f4025
Christian Ho <114313441+ChoBobby@users.noreply.github.com>
2022-10-01 00:44:11 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	int randNum;
+	int userNum = -1;
+	int numGuesses = 0;
+	char play;
+
+	cout << "Welcome to my number guessing game, would you like to play? (Y or N): ";
+	cin >> play;
+
+	if (play == 'y' || play == 'Y') {
+		cout << endl << "Great! Try to guess my number between 0-99. I will tell you if my number is lower or higher than your guess..." << endl;
+	}
+	else if (play == 'n' || play == 'N') {
+		cout << "Have a nice day!" << endl;
+	}
+	else {
+		cout << "Error: Please type 'Y' or 'N'" << endl;
+	}
+
+	srand(time(0));
+	randNum = rand() % 100;
+
+	cout << endl << "...Start guessing!" << endl;
+
+	while (userNum != randNum) {
+		cin >> userNum;
+
+		if (userNum < randNum) {
+			cout << "Higher." << endl << endl;
+		}
+		else if (userNum > randNum) {
+			cout << "Lower." << endl << endl;
+		}
+		else {
+			cout << "You got it!" << endl;
+			cout << "Number of Guesses: " << numGuesses << endl;
+		}
+		++numGuesses;
+	}
+
+	return 0;
+}
